a13bc676761b1321609e4fad6e9f53dda9797df1
HanmingZhang <HanmingZhang@hotmail.com>
2017-10-24 23:04:20 -0400

    backup v1

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ const CLUSTERED_DEFFERED = 'Clustered Deferred';
 const params = {
   renderer: CLUSTERED_FORWARD_PLUS,
   _renderer: null,
+  Bloom: false,
+  ToonShading: false,
 };
 
 setRenderer(params.renderer);
@@ -29,7 +31,30 @@ function setRenderer(renderer) {
   }
 }
 
+function setBloom(){
+  //params.isBloom = !params.isBloom;
+  if(params.Bloom){
+    console.log("Now Bloom is true~!");
+  }
+  else{
+    console.log("Now Bloom is false~!");
+  }
+}
+
+function setToonShading(){
+  //params.isBloom = !params.isBloom;
+  if(params.ToonShading){
+    console.log("Now ToonShading is true~!");
+  }
+  else{
+    console.log("Now ToonShading is false~!");
+  }
+}
+
 gui.add(params, 'renderer', [FORWARD, CLUSTERED_FORWARD_PLUS, CLUSTERED_DEFFERED]).onChange(setRenderer);
+
+gui.add(params, 'Bloom').onChange(setBloom);
+gui.add(params, 'ToonShading').onChange(setToonShading);
 
 const scene = new Scene();
 scene.loadGLTF('models/sponza/sponza.gltf');
