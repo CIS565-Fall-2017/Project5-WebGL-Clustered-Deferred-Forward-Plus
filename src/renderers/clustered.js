330a5053854907ab8e7d2d7c88e0e02a7d1155cc
Fengkai Wu <wufk@Hotmail.com>
2017-10-26 21:53:42 -0400

    fix bug

--- src/renderers/clustered.js
+++ src/renderers/clustered.js
@@ -1,34 +1,8 @@
-import { mat4, vec4, vec3 } from 'gl-matrix';
+import { mat4, vec4, vec3, vec2 } from 'gl-matrix';
 import { NUM_LIGHTS } from '../scene';
 import TextureBuffer from './textureBuffer';
 
-export const MAX_LIGHTS_PER_CLUSTER = 100;
-
-  function distance(light_pos, idx, length) {
-    return (light_pos[idx] - length * light_pos[2]) / Math.sqrt(1 + length * length);
-  }
- 
-  function upper_bound(light_pos, light_radius, num_slices, stride, idx) {
-
-    var hi;
-    for(hi = num_slices; hi >= 0; hi--) {
-      let len = stride * (hi - 1 - num_slices * 0.5);
-      if (distance(light_pos, idx, len) >= -light_radius) {
-        break;
-      }
-    }
-  }
-
-  function lower_bound(light_pos, light_radius, num_slices, stride, idx) {
-    var lo;
-    for(lo = 0; lo < num_slices; lo++) {
-      let len = stride * (lo + 1 - num_slices * 0.5);
-      if (distance(light_pos, idx, len) <= light_radius){
-        break;
-      }
-    }
-    return lo;
-  }
+export const MAX_LIGHTS_PER_CLUSTER = 1000;
 
 export default class ClusteredRenderer {
   constructor(xSlices, ySlices, zSlices) {
@@ -39,7 +13,7 @@ export default class ClusteredRenderer {
     this._zSlices = zSlices;
   }
 
-  updateClusters(camera, viewMatrix, scene) {
+  updateClusters(camera, viewMatrix, projectionMatrix, scene) {
     // TODO: Update the cluster texture with the count and indices of the lights in each cluster
     // This will take some time. The math is nontrivial...
 
@@ -53,55 +27,67 @@ export default class ClusteredRenderer {
       }
     }
 
-    var radian = Math.PI/180.0;
-    var y_len = 2.0 * Math.tan(camera.fov * 0.5 * radian);
-    var y_stride = y_len / this._ySlices;
+    const radian = Math.PI / 180.0;
+    const y_len = Math.tan(camera.fov * 0.5 * radian);
+    const x_len = y_len * camera.aspect;
+    const z_step = (camera.far - camera.near) / this._zSlices;
 
-    var x_len = camera.aspect * y_len;
-    var x_stride = x_len / this._xSlices;
-
-    var z_stride = (camera.far - camera.near) / this._zSlices;
-
-    for (let i = 0; i < NUM_LIGHTS; i++) {
-      let radius = scene.lights[i].radius;
-      let light_pos = vec4.fromValues(scene.lights[i].position[0],
-                                      scene.lights[i].position[1],
-                                      scene.lights[i].position[2],
-                                      1.0);
-      vec4.transformMat4(light_pos, light_pos, viewMatrix);
-      light_pos[2] *= -1.0;
-
-      let x_lo = lower_bound(light_pos, radius, this._xSlices, x_stride, 0);
-      let y_lo = lower_bound(light_pos, radius, this._ySlices, y_stride, 1);
-
-      let x_hi = upper_bound(light_pos, radius, this._xSlices, x_stride, 0);
-      let y_hi = upper_bound(light_pos, radius, this._ySlices, y_stride, 1);
-
-      let shifted_z = light_pos[2] - camera.near;
-      let min_z = shifted_z - radius;
-      let max_z = shifted_z + radius;
-
-      let z_lo = Math.floor(min_z / z_stride);
-      let z_hi = Math.floor(max_z / z_stride) + 1;
-      z_lo = Math.max(z_lo, 0);
-      z_hi = Math.min(z_hi, this._zSlices);
+   for(let i = 0; i < NUM_LIGHTS; i++) {
+       let radius = scene.lights[i].radius;
+       let light_pos = vec4.fromValues(scene.lights[i].position[0], 
+                                      scene.lights[i].position[1], 
+                                      scene.lights[i].position[2], 1.0);
+       vec4.transformMat4(light_pos, light_pos, viewMatrix);
+       light_pos[2] *= -1.0;
+       
+       let light_x_len = y_len*light_pos[2];        
+       let light_y_len = x_len*light_pos[2];
+       let y_step = 2 * light_x_len / this._ySlices;
+       let x_step = 2 * light_y_len / this._xSlices;
+       let lightPosNewY = light_x_len + light_pos[1];
+       let lightPosNewX = light_y_len + light_pos[0];
 
 
-      for(let x = x_lo; x <= x_hi; x++) {
-        for(let y = y_lo; y <= y_hi; y++) {
-          for(let z = z_lo; z <= z_hi; z++) {
-            let cluster_idx = x + y * this._xSlices + z * this._xSlices * this._xSlices;
-            let counts_idx = this._clusterTexture.bufferIndex(cluster_idx, 0);
-            let counts_lights = this._clusterTexture.bufferIndex[counts_idx] + 1;
+       let lightStartY = lightPosNewY - radius;
+       let lightStopY = lightPosNewY + radius;
+       let lightStartX = lightPosNewX - radius;
+       let lightStopX = lightPosNewX + radius;
 
-            if (counts_lights < MAX_LIGHTS_PER_CLUSTER) {
-              this._clusterTexture.buffer[counts_idx] = counts_lights;
-              let light_texel = Math.floor(counts_lights * 0.25);
-              let light_texel_idx = this._clusterTexture.bufferIndex(cluster_idx, light_texel);
+       let y_lo  = Math.floor(lightStartY / y_step); 
+       let y_hi   = Math.floor(lightStopY  / y_step); 
+       let x_lo  = Math.floor(lightStartX / x_step); 
+       let x_hi   = Math.floor(lightStopX  / x_step); 
 
-              let rem = counts_lights - light_texel * 4
-              this._clusterTexture[light_texel_idx + rem] = i;
-            }
+       if(y_lo > this._ySlices-1 || y_hi < 0)  continue; 
+       if(x_lo > this._xSlices-1 || x_hi < 0) { continue; }
+       y_lo = Math.max(0, y_lo);
+       x_lo = Math.max(0, x_lo);
+       y_hi = Math.min(this._ySlices, y_hi);
+       x_hi = Math.min(this._xSlices, x_hi);
+
+       let lightPosNewZ = light_pos[2] - camera.near;
+       let lightStartZ = lightPosNewZ - radius;
+       let lightStopZ = lightPosNewZ + radius; 
+       let z_lo  = Math.floor(lightStartZ / z_step); 
+       let z_hi   = Math.floor(lightStopZ  / z_step)+1;
+       if(z_lo > this._zSlices-1 || z_hi < 0) { continue; }
+       z_lo = Math.max(0, z_lo);
+       z_hi = Math.min(this._zSlices, z_hi);
+
+      for(let x = x_lo; x <= x_hi; ++x) {
+        for(let y = y_lo; y < y_hi; ++y) {
+          for(let z = z_lo; z < z_hi; ++z) {
+             let cluster_idx = x + y*this._xSlices + z*this._xSlices*this._ySlices;
+             let light_num_idx = this._clusterTexture.bufferIndex(cluster_idx, 0);
+             let light_nums = 1 + this._clusterTexture.buffer[light_num_idx];
+
+             if(light_nums < MAX_LIGHTS_PER_CLUSTER) {
+                 this._clusterTexture.buffer[light_num_idx] = light_nums;
+                 let light_texel = Math.floor(light_nums*0.25);
+                 let texel_idx = this._clusterTexture.bufferIndex(cluster_idx, light_texel);
+                 let comp_idx = light_nums - light_texel*4;
+                 this._clusterTexture.buffer[texel_idx+comp_idx] = i;
+              }
           }
         }
       }
