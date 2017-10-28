848160fc211c2f03a39d23271f0a365c2d08016f
wpchop <wpchop@gmail.com>
2017-10-28 14:11:46 -0400

    cluster indexing

--- src/init.js
+++ src/init.js
@@ -60,7 +60,8 @@ stats.domElement.style.top = '0px';
 document.body.appendChild(stats.domElement);
 
 // Initialize camera
-export const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+// Changed camera far-clip to 50
+export const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 50);
 
 // Initialize camera controls
 export const cameraControls = new OrbitControls(camera, canvas);
--- src/renderers/clustered.js
+++ src/renderers/clustered.js
@@ -4,6 +4,20 @@ import TextureBuffer from './textureBuffer';
 
 export const MAX_LIGHTS_PER_CLUSTER = 100;
 
+function getIndex(lightPos, min, max, numSlices) {
+  var z = lightPos;
+  var step = (max - min)/numSlices;
+  console.log("step: ", step);
+  console.log("light position z: ", z);
+  if (z < min) {
+    return -1;
+  } else if (z > max) {
+    return 15;
+  } else {
+    return (Math.floor((z - min)/ step));
+  }
+}
+
 export default class ClusteredRenderer {
   constructor(xSlices, ySlices, zSlices) {
     // Create a texture to store cluster data. Each cluster stores the number of lights followed by the light indices
@@ -27,6 +41,73 @@ export default class ClusteredRenderer {
       }
     }
 
+    // For each light
+    // 1. Get bounding indices for x, y z
+    // 2. Add light to clusterTexture for indices between
+    for (let l = 0; l < NUM_LIGHTS; l++) {
+      let lightPos = vec4.create();
+      lightPos[0] = scene.lights[l].position[0];
+      lightPos[1] = scene.lights[l].position[1];
+      lightPos[2] = scene.lights[l].position[2];
+      lightPos[3] = 1;
+
+      let r = scene.lights[l].radius;
+
+      // Light position in camera space
+      vec4.transformMat4(lightPos, lightPos, viewMatrix);
+
+      let posZ = -lightPos[2]; 
+      let posY = lightPos[1]; 
+      let posX = lightPos[0];
+      
+      // y = z * tan(FOV)
+      // max yPosition at z plane of intersection
+      let maxPosY = posZ * Math.tan(Math.PI/180 * camera.fov * 0.5);
+      let minPosY = - maxPosY;
+
+      let maxPosX = maxPosY* camera.aspect;
+      let minPosX = -maxPosX; 
+
+      let minX = getIndex(posX - r, minPosX, maxPosX, this._xSlices);
+      let maxX = getIndex(posX + r, minPosX, maxPosX, this._xSlices);
+
+      let minY = getIndex(posY - r, minPosY, maxPosY, this._ySlices);
+      let maxY = getIndex(posY + r, minPosY, maxPosY, this._ySlices);
+
+      let minZ = getIndex(posZ - r, camera.near, camera.far, this._zSlices);
+      let maxZ = getIndex(posZ + r, camera.near, camera.far, this._zSlices);
+
+      if (minX == 15 || minY == 15 || minZ == 15 
+        || maxY == -1 || maxX == -1 || maxZ == -1) {
+          continue;
+        }
+
+      minX = Math.max(minX, 0);
+      maxX = Math.min(maxX, this._xSlices - 1);
+      minY = Math.max(minY, 0);
+      maxY = Math.min(maxY, this._ySlices - 1);
+      minZ = Math.max(minZ, 0);
+      maxZ = Math.min(maxZ, this._zSlices - 1);
+
+      for (let k = minZ; k < maxZ; k++) {
+        for (let j = minY; j < maxY; j++) {
+          for (let i = minX; i < maxX; i++) {
+            let idx = i + j * this._xSlices + k * this._xSlices * this._ySlices;
+            this._clusterTexture[this._clusterTexture.bufferIndex(idx, 0)]+= 1;
+            let numLights = this._clusterTexture[this._clusterTexture.bufferIndex(idx, 0)];
+            this._clusterTexture[this._clusterTexture.bufferIndex(idx, numLights)] = l;
+          }
+        } 
+      }
+      
+
+    }
+
+    
+
     this._clusterTexture.update();
   }
+
+
+
 }
